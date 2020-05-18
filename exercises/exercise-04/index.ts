05fde7db5e64a70bf706fee9547362d929d478f7
Mike Chen <mhchen@gmail.com>
2020-05-17 23:56:02 -0700

    Exercise 4 bonus

--- exercises/exercise-04/index.ts
+++ exercises/exercise-04/index.ts
@@ -95,9 +95,11 @@ function logPerson(person: Person) {
     console.log(` - ${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`);
 }
 
-function filterUsers(persons: Person[], criteria: Partial<User>): User[] {
+type UserFilter = Omit<Partial<User>, 'type'>;
+
+function filterUsers(persons: Person[], criteria: UserFilter): User[] {
     return persons.filter(isUser).filter((user) => {
-        let criteriaKeys = Object.keys(criteria) as (keyof User)[];
+        let criteriaKeys = Object.keys(criteria) as (keyof UserFilter)[];
         return criteriaKeys.every((fieldName) => {
             return user[fieldName] === criteria[fieldName];
         });
@@ -109,7 +111,7 @@ console.log(chalk.yellow('Users of age 23:'));
 filterUsers(
     persons,
     {
-        age: 23
+        age: 23,
     }
 ).forEach(logPerson);
 
