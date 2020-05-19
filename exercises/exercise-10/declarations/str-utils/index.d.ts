09e529e5a18fe30d58ad72a047f63edb688452ad
Mike Chen <mhchen@gmail.com>
2020-05-18 23:59:12 -0700

    Exercise 10

--- exercises/exercise-10/declarations/str-utils/index.d.ts
+++ exercises/exercise-10/declarations/str-utils/index.d.ts
@@ -1,4 +1,9 @@
+type StringManipulator = (str: string) => string;
 declare module 'str-utils' {
-    // export const ...
+    export const strReverse: StringManipulator;
+    export const strToLower: StringManipulator;
+    export const strToUpper: StringManipulator;
+    export const strRandomize: StringManipulator;
+    export const strInvertCase: StringManipulator;
     // export function ...
 }
